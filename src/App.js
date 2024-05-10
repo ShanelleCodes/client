c8301ff45728d93979664395fc8a643e1f2b28a2
Shanelle Codes <shanelle.codes@gmail.com>
2024-05-09 20:56:42 -0400

    create components and pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
+import { useEffect } from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  useLocation
+} from 'react-router-dom';
+import Home from "./scenes/home/Home.jsx";
+import ItemDetails from "./scenes/itemDetails/ItemDetails.jsx";
+import Checkout from "./scenes/checkout/Checkout";
+import Confirmation from "./scenes/checkout/Confirmation";
+import NavBar from "./scenes/global/Navbar.jsx";
+import CartMenu from "./scenes/global/CartMenu.jsx";
 
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname])
+
+  return null;
+}
 
 function App() {
   return (
     <div className="app">
-      
+      <BrowserRouter>
+      <NavBar />
+      <scrollToTop />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="item/:itemId" element={<ItemDetails />} />
+        <Route path="checkout" element={<Checkout />} />
+        <Route path="checkout/success" element={<Confirmation />} />
+      </Routes>
+      <CartMenu />
+      </BrowserRouter>
     </div>
   );
 }
